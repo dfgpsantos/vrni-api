a3eb338b2c1afe6455aab08d9f5a6dde598f7f09
David Santos <63432530+dfgpsantos@users.noreply.github.com>
2022-04-28 20:14:50 -0300

    Create token.sh

---
+++ token.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#please remove your credentials info after using the script
+
+rm -rf *.txt
+
+cat > user.txt << EOL
+{
+  "username": "userhere@domainhere.com",
+  "password": "yourpasswordhere",
+  "domain": {
+    "domain_type": "LDAP",
+    "value": "vmware.com"
+  }
+}
+EOL
+
+curl -i -s -k -b cookie.txt -D header.txt -H "Content-Type: application/json" --data @user.txt -X POST https://$VRNI/api/ni/auth/token | grep token > token.txt
+
+rm -rf user.txt
+
+cat token.txt
